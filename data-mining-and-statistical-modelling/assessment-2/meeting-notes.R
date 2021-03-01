451946b944ff53b1ce94c538cefc6f99c8445bfd
Mark <markbaber1992@live.co.uk>
2021-03-01 15:20:27 +0000

    cried a lot during DM

--- data-mining-and-statistical-modelling/assessment-2/meeting-notes.R
+++ data-mining-and-statistical-modelling/assessment-2/meeting-notes.R
@@ -49,32 +49,62 @@ Data <- list("Tmax" = Tmax.data,
                "Tmin" = Tmin.data
              )
 # select the Tmax from Northern_Ireland
-Data[["Tmax"]][["Northern_Ireland"]]
+Data$Tmax$Northern_Ireland
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 2 - Task 2
 # find max temp
 
-maxTemp <- function(list, Data){
-  
-  
-  
-}
-
-# change the data into 
-
-Data[["Tmax"]] %>% # only select 
-  unlist() %>% # unlist it
+# find the max value's index
+maxIndex <- Data$Tmax %>% 
+  unlist() %>% 
   as.vector() %>% 
   which.max() 
 
-Data[["Tmax"]][]
-  
-Data %>% # get Data
-  unlist() %>% # unlist it
-  which.min()
+# find sub position
+subIndex <- round((maxIndex/16440)*10) # 8 - This could be district
+
+# find max value
+# maxVal <- DTmUn %>% which.max()
+
+# find year
+# maxYear <- floor(time(DTmUn))[which.max(DTmUn)]
+
+# month
+# maxMonth <- month.abb[(time(Data$Tmax)[which.max(Data$Tmax)] %>% 1)*12+1]
+
+# calculate regions
+# maxRegions <- names(Data$Tmax)
+
+dTmax <- Data$Tmax %>%
+  unlist() %>% 
+  as.vector() %>% 
+  which.max()
+
+# GETS MONTH
+unlistedTMAX <- Data$Tmax %>% unlist()
+myMm <- month.abb[(time(unlistedTMAX)[which.min(unlistedTMAX)] %% 1)*12+1]
+# plot(Data$Tmax$Northern_Ireland, type = 'l')
+
+# get max temp of Tmax
+sapply(Data$Tmax, which.max)
+
+# data_min_value_time <- time(Data)[which.min(Data)]
+
+# unlist tMAX
+tmax_unl <- Data$Tmax %>% 
+  unlist() %>% 
+  as.vector()
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3 - Task 3
 
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 4 - Task 4
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 5 - Task 5
 
 # functions for all the models
 # get the AIC reading for each one to see which is best.
@@ -91,3 +121,40 @@ Data %>% # get Data
 # might have to nest 3 lapplies for each things?!
 
 # problem could be seasionalities with p values (<0.01)
+
+# num to month
+numyear2monthyear <- function(x){   
+  c(trunc(x),                   # entire part = year
+    round((x-floor(x))*12 + 1)) # decimal part * 12 + 1 (Jan=0) = Month
+}
+
+# try test function on Data$Tmax
+numyear2monthyear(Data$Tmax$Northern_Ireland)
+numyear2monthyear(tmax_unl)
+
+# try it with lapply
+lapply(Data$Tmax, numyear2monthyear)
+
+# yeet
+extrema_dates <- function(ts){
+  ts_min_date <- numyear2monthyear(time(ts)[which.min(ts)])
+  ts_max_date <- numyear2monthyear(time(ts)[which.max(ts)])
+  list(min=min(ts),
+       min_year=ts_min_date[1],
+       min_month=ts_min_date[2],
+       max=max(ts),
+       max_year=ts_max_date[1],
+       max_month=ts_max_date[2])
+}
+
+extrema_dates(Data$Tmax$Northern_Ireland)
+extrema_dates(tmax_unl)
+# try to use the 2 above functions
+sapply(Data$Tmax, extrema_dates)
+
+
+#which.max(Data$Tmax)
+
+numyear2monthyear(time(data)[which.max(data)])
+
+numyear2monthyear(time(data)[which.min(data)])
--- data-mining-and-statistical-modelling/assessment-2/notes.R
+++ data-mining-and-statistical-modelling/assessment-2/notes.R
@@ -25,3 +25,31 @@ plot(globaltemps, type = 'l')
 lines(create.model("globaltemps", "linear"),    col = 'green',  lwd = 3)
 lines(create.model("globaltemps", "quadratic"), col = 'purple', lwd = 3)
 lines(create.model("globaltemps", "cubic"),     col = 'red',    lwd = 3)
+
+
+
+highest_temp <- function(sublist){
+  # what position is the max value in the nested list
+  sublist %>% 
+    unlist() %>% 
+    as.vector() %>% 
+    which.max() -> position
+  # what position does this correspond to in the sublist
+  sub_position <- round((position/16440)*10)
+  # find the value, year, month then region
+  # VAL
+  max_val <- 
+    sublist[[sub_position]][which.max(sublist[[sub_position]])]
+  # YEAR
+  max_year <- 
+    floor(time(sublist[[sub_position]]))[which.max(sublist[[sub_position]])]
+  # MONTH
+  month <- 
+    month.abb[(time(sublist[[sub_position]])[which.max(sublist[[sub_position]])] %>% 1)*12+1]
+  # REGION
+  region <- names(sublist[sub_position])
+  # PRINT
+  paste('max temp is', max_val, 'during', month, max_year, 'in', region, sep = " ") %>% print()
+}
+
+highest_temp(Data)
